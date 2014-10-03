1f7c342005bd57562916550d32a6a85ff005fc1c
pikesley <github@orgraphone.org>
2014-10-03 21:34:13 +0100

    Intervals

--- lib/crunch/date.rb
+++ lib/crunch/date.rb
@@ -3,4 +3,20 @@ module Crunch
     d = DateTime.parse raw_date
     d.strftime "%A the #{d.day.ordinalize} of %B"
   end
+
+  def self.time_since past_date
+    now = DateTime.now
+    i = (now - Date.parse(past_date)).to_i
+
+    return 'in the future' if i < 0
+    return 'today' if i == 0
+    return 'yesterday' if i == 1
+
+    d = i == 1 ? 'day' : 'days'
+
+    '%d %s ago' % [
+      i,
+      d
+    ]
+  end
 end
--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -5,5 +5,17 @@ module Crunch
     it 'should format a date correctly' do
       expect(Crunch.date_fixer '2014-06-15').to eq 'Sunday the 15th of June'
     end
+
+    it 'should calculate an interval correctly' do
+      Timecop.freeze '2014-10-03T00:00:00+00:00'
+
+      expect(Crunch.time_since '2014-10-03').to eq 'today'
+      expect(Crunch.time_since '2014-10-02').to eq 'yesterday'
+      expect(Crunch.time_since '2014-10-01').to eq '2 days ago'
+      expect(Crunch.time_since '2014-06-15').to eq '110 days ago'
+      expect(Crunch.time_since '2015-01-01').to eq 'in the future'
+
+      Timecop.return
+    end
   end
 end
