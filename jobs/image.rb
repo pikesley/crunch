3e25e009256a0dedd4bf1a0ba90cb70fd6527cd5
pikesley <github@orgraphone.org>
2014-10-04 18:15:01 +0100

    Better Flickr

--- jobs/image.rb
+++ jobs/image.rb
@@ -4,5 +4,7 @@ SCHEDULER.every '5m', :first_at => Time.now do
   FlickRaw.api_key = ENV['FLICKR_API_KEY']
   FlickRaw.shared_secret = ENV['FLICKR_SECRET']
 
-  send_event('picture', {image: FlickRaw.url_n(flickr.photos.getInfo(photo_id: flickr.photosets.getPhotos(photoset_id: ENV['FLICKR_ALBUM_ID'])['photo'][-1]['id']))})
+  f = flickr.photos.getInfo(photo_id: flickr.photosets.getPhotos(photoset_id: ENV['FLICKR_ALBUM_ID'])['photo'].sample['id'])
+
+  send_event('picture', { image: FlickRaw.url(f), date: Crunch.date_fixer(f['dates']['taken']), interval: Crunch.time_since(f['dates']['taken']) } )
 end
