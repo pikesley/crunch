716a73f9acd1b4e69b87d3a7cda00161e806c55b
pikesley <github@orgraphone.org>
2014-10-13 09:46:15 +0100

    Oops, need to handle when she doesn't eat

--- jobs/google_spreadsheet.rb
+++ jobs/google_spreadsheet.rb
@@ -7,7 +7,8 @@ SCHEDULER.every '5m', :first_at => Time.now do
   session = GoogleDrive.login(ENV['GOOGLE_USER'], ENV['GOOGLE_PASS'])
   sheet = session.spreadsheet_by_key(ENV['SPREADSHEET_ID'])
   feeds = sheet.worksheets[0]
-  meal = Crunch::Meal.new feeds.rows[-1]
+#  meal = Crunch::Meal.new feeds.rows[-1]
+  meal = Crunch::Meal.new feeds.rows.reverse.select { |r| r[3].to_i > 0 }[0]
 
   send_event('meal', { food: meal.menu, date: meal.date, interval: meal.time_since })
 
