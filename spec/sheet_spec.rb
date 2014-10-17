0648f26c817a2ac9a202ffed1260cb9aedb67afc
pikesley <github@orgraphone.org>
2014-10-17 23:25:03 +0100

    This was never going to work

--- spec/sheet_spec.rb
+++ spec/sheet_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-module Crunch
-  describe Sheet do
-    it 'should have sheets', :vcr do
-      sheet = Sheet.new
-    end
-  end
-end
+#module Crunch
+#  describe Sheet do
+#    it 'should have sheets', :vcr do
+#      sheet = Sheet.new
+#    end
+#  end
+#end
